eba6397bbc9215d72c77b7cc99a3879fdc01f994
CedricLt44 <cedric.letutour@gmail.com>
2024-12-16 20:55:35 +0100

    admin et ux design debug et set up

--- projet/admin.py
+++ projet/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Project
+
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_by', 'created_at') 
+    search_fields = ('name', 'created_at','created_by')  
+    list_filter = ('name', 'created_at','created_by')  
+
+admin.site.register(Project, ProjectAdmin)
